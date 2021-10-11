c986e59ff837943dd0551609c07ed618a71a299f
Võ Ngọc Thắng <vongocthang160199@gmail.com>
2021-10-11 18:34:53 +0700

    create line

---
+++ Box Move Target/Assets/Scripts/CarControl.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarControl : MonoBehaviour
+{
+    public Rigidbody2D rb2D;
+    public Vector3 forceLeft;
+    public Vector3 forceRight;
+    public float speed;
+
+    public bool moveLeft;
+    public bool moveRight;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Move();
+        NotMove();
+    }
+
+    public void MoveLeft()
+    {
+        moveLeft = true;
+    }
+
+    public void MoveRight()
+    {
+        moveRight = true;
+    }
+
+    public void Move()
+    {
+        if (moveLeft == true)
+        {
+            rb2D.velocity =new Vector2(-speed * Time.deltaTime, 0f);
+        }
+        if (moveRight == true)
+        {
+            
+            rb2D.velocity = new Vector2(speed * Time.deltaTime, 0f);
+        }
+    }
+
+    public void NotMove()
+    {
+        if (moveLeft == true || moveRight == true)
+        {
+            if (Input.GetMouseButtonUp(0))
+            {
+                moveLeft = false;
+                moveRight = false;
+            }
+        }
+    }
+}
