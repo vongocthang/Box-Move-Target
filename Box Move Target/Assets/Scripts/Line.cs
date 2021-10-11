c250c161add42db5ebefd2e506b39e1873459c7e
Võ Ngọc Thắng <vongocthang160199@gmail.com>
2021-10-11 16:22:50 +0700

    is working

---
+++ Box Move Target/Assets/Scripts/Line.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Line : MonoBehaviour
+{
+    public LineRenderer lineRenderer;
+    public EdgeCollider2D edgeCollider;
+    public Rigidbody2D rb2D;
+
+    public List<Vector2> points = new List<Vector2>();
+    public int pointsCount = 0;
+
+    float pointsMinDistance = 0.1f;
+
+    public void AddPoints(Vector2 newPoint)
+    {
+        if (pointsCount >= 1 && Vector2.Distance(newPoint, GetLastPoint()) < pointsMinDistance)
+        {
+            return;
+        }
+
+        points.Add(newPoint);
+        pointsCount++;
+
+        lineRenderer.positionCount = pointsCount;
+        lineRenderer.SetPosition(pointsCount - 1, newPoint);
+
+        if (pointsCount > 1)
+        {
+            edgeCollider.points = points.ToArray();
+        }
+    }
+
+    public Vector2 GetLastPoint()
+    {
+        return (Vector2) lineRenderer.GetPosition(pointsCount - 1);
+    }
+
+    public void UsePhysics(bool usePhysics)
+    {
+        rb2D.isKinematic = !usePhysics;
+    }
+
+    public void SetLineColor(Gradient gradientColor)
+    {
+        lineRenderer.colorGradient = gradientColor;
+    }
+
+    public void SetPointMinDistance(float distance)
+    {
+        pointsMinDistance = distance;
+    }
+
+    public void SetLineWidth(float width)
+    {
+        lineRenderer.startWidth = width;
+        lineRenderer.endWidth = width;
+
+        edgeCollider.edgeRadius = width / 2f;
+    }
+}
---
+++ Box Move Target/Assets/Scripts/LineDraw.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LineDraw : MonoBehaviour
+{
+    public GameObject linePrefab;
+
+    [Space(30f)]
+    public Gradient lineColor;
+    public float linePointMinDistance;
+    public float lineWidth;
+
+    Line currentLine;
+
+    Camera cam;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        cam = Camera.main;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            BeginDraw();
+        }
+        if (currentLine != null)
+        {
+            Draw();
+        }
+        if (Input.GetMouseButtonUp(0))
+        {
+            EndDraw();
+        }
+        
+    }
+
+    public void BeginDraw()
+    {
+        currentLine = Instantiate(linePrefab, this.transform).GetComponent<Line>();
+
+        currentLine.UsePhysics(false);
+        currentLine.SetLineColor(lineColor);
+        currentLine.SetPointMinDistance(linePointMinDistance);
+        currentLine.SetLineWidth(lineWidth);
+    }
+
+    public void Draw()
+    {
+        Vector2 mousePos = cam.ScreenToWorldPoint(Input.mousePosition);
+        currentLine.AddPoints(mousePos);
+    }
+
+    public void EndDraw()
+    {
+        if (currentLine != null)
+        {
+            if (currentLine.pointsCount < 2)
+            {
+                //Nét vẽ không thể là một điểm
+                Destroy(currentLine.gameObject);
+            }
+            else
+            {
+                currentLine.UsePhysics(true);
+                currentLine = null;
+            }
+        }
+    }
+}
